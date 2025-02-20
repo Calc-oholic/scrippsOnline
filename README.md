eb3e8f87bfa444136c0a85c43fc44fa40c9930cd
Calc-oholic <104602895+Calc-oholic@users.noreply.github.com>
2025-02-19 23:08:06 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+# Spelling UIL Study Site
+
+A simple and intuitive web application that helps you study Spelling UIL words by pronouncing them for you while you type, and instantly checking your spelling. Select the word set you'd like to study and start improving your spelling skills right away!
+
+**Note:** As this application is hosted on Vercel, only one person can use it at a time, and so is an example. If you wish to have 24/7 access and a personal instance, please fork this repository and follow the setup instructions below.
+
+## Features
+
+- Pronunciation of Spelling UIL words while typing
+- Instant spell-check functionality
+- On-screen accent keyboard
+- Choose from multiple word sets to study
+
+## Setup Instructions
+
+
+**1.** Fork the repo and name it something like "YOURSCHOOLHERE-Spelling-UIL".\
+**2.** Sign up on Vercel using your Github account (or add your Github account if you already have a Vercel account).\
+**3.** Click on "Add New..." at the top right corner, then Project.\
+**4.** Then select the "Import" button next to the repository you forked made earlier.\
+**5.** Name the project the same as the repo, and set the framework preset to "Other".\
+**6.** Click deploy, and enjoy!
+
+## Roadmap
+
+- Additional browser support
+- Add another voice for pronunciations
+
+## Badges
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+
+## Authors
+
+- [@Calc-oholic](https://www.github.com/Calc-oholic)
+- [@FollowedPlate](https://github.com/FollowedPlate)
+- [@A-Singh-er](https://www.github.com/A-Singh-er)
+- [Argyle HS Spelling Team](https://ahs.argyleisd.com)
---
+++ api/index.py
@@ -0,0 +1,199 @@
+from flask import Flask, render_template, request, redirect, url_for, send_file
+import random
+from gtts import gTTS
+import os
+import tempfile
+import io
+import time
+
+app = Flask(__name__)
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+# Load word list
+def load_word_list(filename):
+    directory_path = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(directory_path, filename)
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            return [line.strip() for line in file]
+    except FileNotFoundError:
+        print(f"File '{filename}' not found in '{directory_path}'.")
+        return []
+    except IOError as e:
+        print(f"Error reading file '{filename}': {e}")
+        return []
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        return []
+
+current_word_idx = 0
+main_contest_words = []
+wrong_words = []
+
+# Select words for the contest
+def select_words(word_list, start_index, end_index, num_words):
+    if 1 <= start_index <= end_index <= len(word_list):
+        selected_words = word_list[start_index - 1:end_index]
+        random.shuffle(selected_words)
+        return selected_words[:num_words]
+    else:
+        return []
+
+# Generate and play word pronunciation
+def generate_and_play_word(word):
+    tts = gTTS(text=word, lang='en')
+    current_time = int(time.time())
+    temp_file = tempfile.NamedTemporaryFile(suffix=f"_{word}_{current_time}.mp3", delete=False)
+    temp_file.close()
+    tts.save(temp_file.name)
+
+    audio_data = open(temp_file.name, 'rb').read()
+
+    try:
+        os.remove(temp_file.name)
+    except PermissionError:
+        pass
+
+    return audio_data
+
+# Generate and play word pronunciation
+def generate_and_play_word_alternate(word):
+    tts = gTTS(text=word, lang='en', tld='com.ng')
+    current_time = int(time.time())
+    temp_file = tempfile.NamedTemporaryFile(suffix=f"_{word}_{current_time}.mp3", delete=False)
+    temp_file.close()
+    tts.save(temp_file.name)
+
+    audio_data = open(temp_file.name, 'rb').read()
+
+    try:
+        os.remove(temp_file.name)
+    except PermissionError:
+        pass
+
+    return audio_data
+
+
+# Check user input against the correct word
+def check_word(user_input):
+    global current_word_idx, main_contest_words
+
+    if current_word_idx < len(main_contest_words):
+      
+        # check = main_contest_words[current_word_idx]
+        # startPare = check.find('(')
+        # checkFix = check
+        # if startPare > 0:
+        #     checkFix = check[0:startPare-1]
+        # else:
+        #     checkFix = check
+        checkFix = main_contest_words[current_word_idx].split("(")[0].strip()
+        
+        correct_words = [word.strip() for word in checkFix.split(",")]
+        
+        user_inputs = [input.strip() for input in user_input.split(",")]
+
+        if all(input in correct_words for input in user_inputs):
+            return True
+        else:
+            feedback = f"Incorrect. Correct answer: '{', '.join(correct_words)}'"
+            return feedback
+    return False
+
+# Route for the home page
+@app.route("/", methods=["GET", "POST"])
+def index():
+    global current_word_idx, main_contest_words, wrong_words
+
+    file_names = ["2024.txt", "2023.txt", "2022.txt", "2021.txt", "2020.txt", "2019.txt"]
+
+    if request.method == "POST":
+        filename = request.form["filename"]
+        start_index = int(request.form["start_index"])
+        end_index = int(request.form["end_index"])
+        num_words = int(request.form["num_words"])
+        word_list = load_word_list(filename)
+
+        if not word_list:
+            return render_template("index.html", file_names=file_names, error_message=f"Failed to load word list from '{filename}'.")
+
+        main_contest_words = select_words(word_list, start_index, end_index, num_words)
+        
+        if not main_contest_words:
+            return render_template("index.html", file_names=file_names, error_message=f"Failed to select words from '{filename}'. Please check your indices.")
+
+        current_word_idx = 0
+        wrong_words = []
+
+        audio_data = generate_and_play_word(main_contest_words[current_word_idx])
+        return render_template("contest.html", current_word_idx=current_word_idx, total_words=len(main_contest_words), feedback=None, audio_data=audio_data, file_names=file_names)
+
+    return render_template("index.html", file_names=file_names)
+
+# Inside the /contest route
+@app.route("/contest", methods=["GET", "POST"])
+def contest():
+    global current_word_idx, main_contest_words, wrong_words
+
+    if request.method == "POST":
+        user_input = request.form["user_input"]
+        feedback = check_word(user_input)
+
+        if feedback == True:
+            current_word_idx += 1
+
+            if current_word_idx < len(main_contest_words):
+                audio_data = generate_and_play_word(main_contest_words[current_word_idx])
+                timestamp = int(time.time())
+                audio_url = f"/pronounce?timestamp={timestamp}"
+
+                return render_template("contest.html", current_word_idx=current_word_idx, total_words=len(main_contest_words), feedback=feedback, audio_data=audio_data, audio_url=audio_url, wrong_words=wrong_words)
+
+        else:
+            wrong_words.append((main_contest_words[current_word_idx], user_input))
+
+        if current_word_idx < len(main_contest_words):
+            audio_data = generate_and_play_word(main_contest_words[current_word_idx])
+            timestamp = int(time.time())
+            audio_url = f"/pronounce?timestamp={timestamp}"
+
+            return render_template("contest.html", current_word_idx=current_word_idx, total_words=len(main_contest_words), feedback=feedback, audio_data=audio_data, audio_url=audio_url, wrong_words=wrong_words)
+        else:
+            return redirect(url_for("index"))
+
+    if current_word_idx < len(main_contest_words):
+        audio_data = generate_and_play_word(main_contest_words[current_word_idx])
+        timestamp = int(time.time())
+        audio_url = f"/pronounce?timestamp={timestamp}"
+
+        return render_template("contest.html", current_word_idx=current_word_idx, total_words=len(main_contest_words), feedback=None, audio_data=audio_data, audio_url=audio_url, wrong_words=wrong_words)
+    else:
+        return redirect(url_for("index"))
+
+@app.route("/pronounce")
+def pronounce_word():
+    global current_word_idx, main_contest_words
+
+    if current_word_idx < len(main_contest_words):
+        word = main_contest_words[current_word_idx]
+        audio_data = generate_and_play_word(word)
+        unique_id = int(time.time())
+        return send_file(io.BytesIO(audio_data), mimetype='audio/mpeg', as_attachment=True, download_name=f'pronunciation_{unique_id}.mp3')
+    else:
+        return send_file(io.BytesIO(b""), mimetype='audio/mpeg', as_attachment=True, download_name='pronunciation_placeholder.mp3')
+
+@app.route("/altpronounce")
+def alt_pronounce_word():
+    global current_word_idx, main_contest_words
+
+    if current_word_idx < len(main_contest_words):
+        word = main_contest_words[current_word_idx]
+        audio_data = generate_and_play_word_alternate(word)
+        unique_id = int(time.time())
+        return send_file(io.BytesIO(audio_data), mimetype='audio/mpeg', as_attachment=True, download_name=f'pronunciation_{unique_id}.mp3')
+    else:
+        return send_file(io.BytesIO(b""), mimetype='audio/mpeg', as_attachment=True, download_name='pronunciation_placeholder.mp3')
+
+if __name__ == "__main__":
+    app.run(debug=True)
